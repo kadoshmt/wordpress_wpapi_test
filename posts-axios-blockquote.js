ab8074a298f4f058c0be972f1dd1bfc49d3ae212
Janes Roberto <kadoshmt2@gmail.com>
2020-08-15 11:23:29 -0400

    shore: split content in verses and choruses

---
+++ posts-axios-blockquote.js
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const axios = require('axios'); 
+const getSortOrder = require('./getSortOrder.js');
+
+/** VARIABLES */
+// The website have 640 posts
+const totalPages = 7; 
+const filename = './wp-posts-blockquote.json';
+const promises = [];
+let posts = [];
+let postsList = [];
+
+// Open file and get data if exists
+if (fs.existsSync(filename)) { 
+  posts = require(filename);
+  postsList = posts.map(ele => ele.title);  
+}
+
+// Async function to get the posts from Wordpress
+const getPosts = async (page) => {    
+  try {
+    const response = await axios.get(
+      `https://harpacrista.org/wp-json/wp/v2/posts?page=${page}&per_page=100`
+    )    
+
+    const result = response.data.map(post => {
+      const {
+        id:number,
+        title:{rendered: title},
+        content:{rendered : body},
+        slug 
+      } = post;    
+
+      let temp = '';
+      let content = '';
+  
+      console.log(number, title);
+
+      // Hymn 637 have a youtube video object. So, let's remove it!
+      if (number === 637) {
+        console.log(`Hino ${number} - ${title} com audio vídeo do youtube`);
+        temp = body.split("</object><br />");
+        // get the body of the hymns
+        content = temp[1];     
+      }
+      // some hymns have the audio tag inside the fist <p>
+      // In this cases, only remove the the first <p>
+      else if(body.search("</audio></p>") === -1){
+        // remove 1st audio tag
+        temp = body.split("</audio><br />");    
+
+        // check if exists 2nd or 3rd audio tags, if then, remove this 
+        if(temp.length === 3) {        
+          temp = '<p>'.concat(temp[1]).concat(temp[2]);
+          temp = temp.split("</a>");
+          temp = '<p>'.concat(temp[1]);
+        // some hymns, like hymn 227 have 3 audio tags 
+        // (https://harpacrista.org/hino/227-deus-amou-de-tal-maneira/)
+        } else {
+          temp = '<p>'.concat(temp[temp.length - 1]) 
+        }      
+        
+        temp = temp.split("<p>Autor");  
+      
+        // get the body of the hymns
+        content = temp[0];
+
+      } else {
+        console.log(`Hino ${number} - ${title} com audio dentro da tag <p>`);
+        temp = body.split("</audio></p>");     
+        // get the body of the hymns
+        content = temp[1];      
+      }   
+
+      // get the authors info
+      temp = '<p>Autor'.concat(temp[1]);    
+      temp = temp.split("tor:");   
+      temp = temp[1].trim().split("<br />");
+      const author = temp[0].trim();
+      const full_content = content;
+      let num_chorus = 0;
+      const chorus = []; 
+
+      // Separate chorus from verses (if exists)
+      if(body.search("<blockquote>") !== -1){     
+        temp = content.split("<blockquote><p>");        
+        content = temp[0].concat("[[CHORUS-1]]");
+        // some hymns, like hymn 32 or 525 have 2 chorus
+        // (https://harpacrista.org/hino/525-vencendo-vem-jesus/)      
+        if(temp.length === 3) {                    
+          let temp2 = temp[2];                  
+          temp = temp[1].split("</p></blockquote>");
+          chorus.push(temp[0]);
+          //if(number === 32) console.log(temp[1]);
+          content = content.concat(temp[1]).concat("[[CHORUS-2]]")          
+          chorus.push(temp2.replace("</p></blockquote>",""));          
+          num_chorus = 2;
+        } else {
+          temp = temp[1].split("</p></blockquote>");
+          chorus.push(temp[0]);
+          content = content.concat(temp[1]) 
+          num_chorus = 1;
+        }
+      }      
+
+      return { number, title, slug, author, full_content, content, chorus, num_chorus };
+    })
+
+    // Verify if the title already exists in array. If not, save the post 
+    result.forEach(element => {
+      if(!postsList.includes(element.title)){
+        postsList.push(element.title);
+        posts.push(element);
+      } 
+    });
+  } catch (error) {
+    console.log(page, 'error');
+  }
+}
+
+// put the async calls for the function in a stack of promisses
+for(i = 1; i <= totalPages; i++){
+  promises.push(getPosts(i));
+}
+
+// Run all the promisses
+const runFunc = async () => {await Promise.all(promises)};
+runFunc();
+
+// Wait some time to finish the promisses... 
+setTimeout(()=>{  
+  posts.sort(getSortOrder("number"));  
+  // and save data on file in server
+  fs.writeFile(filename, JSON.stringify(posts, null, 2), err => {
+    if(err) throw new Error ('something went wrong');
+    console.log('Well Done!');
+  });
+}, 10000);
